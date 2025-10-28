47e79b1a81ee3e270e3c1f4a1f546bd655bdab25
Lukasz Paczek <yourf4te@gmail.com>
2025-10-28 21:09:49 +0100

    added kafkaConfig and conditional on property

---
+++ src/main/java/com/paczek/demo/app/config/KafkaConfig.java
@@ -0,0 +1,39 @@
+package com.paczek.demo.app.config;
+
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@ConditionalOnProperty(prefix = "kafka", name = "enabled", havingValue = "true")
+public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String kafkaUrl;
+
+    @Bean
+    @ConditionalOnProperty(prefix = "kafka", name = "enabled", havingValue = "true")
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaUrl);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "kafka", name = "enabled", havingValue = "true")
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+}
--- src/main/java/com/paczek/demo/app/kaffka/KafkaConsumerService.java
+++ src/main/java/com/paczek/demo/app/kaffka/KafkaConsumerService.java
@@ -1,9 +1,11 @@
 package com.paczek.demo.app.kaffka;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@ConditionalOnProperty(prefix = "kafka", name = "enabled", havingValue = "true")
 public class KafkaConsumerService {
 
     @KafkaListener(topics = "demo-topic", groupId = "demo-group")
